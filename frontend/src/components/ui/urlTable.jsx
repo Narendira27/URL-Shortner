acccab9f8d46f6959bc6849f91352df6663779cc
narenidra <narendira2701@gmail.com>
2024-04-17 14:07:29 +0530

    fix responsiveness

--- frontend/src/components/ui/urlTable.jsx
+++ frontend/src/components/ui/urlTable.jsx
@@ -21,14 +21,14 @@ export default function UrlTable({ details, deleteFn, toastFn }) {
     }
 
     return (
-        <TableBody>
+        <TableBody >
             <TableRow>
-                <TableCell className="font-medium "><a href={`${url}get/${shortId}/`} target="__blank" >{shortId}</a>
-                    <button onClick={copyShortUrl} className="ml-2 font-medium"><FaRegCopy /></button></TableCell>
-                <TableCell className="text-center "><a href={`${Url}`} target="__blank" >{Url}</a>
-                    <button onClick={copyUrl} className="ml-2 font-medium"><FaRegCopy /></button></TableCell>
-                <TableCell className="text-center ">{Clicks}</TableCell>
-                <TableCell onClick={() => { deleteFn(shortId) }} className="text-right text-lg md:text-xl cursor-pointer"> <button className="hover:bg-red-500 p-2  rounded-3xl"><MdOutlineDeleteOutline /></button></TableCell>
+                <TableCell className="text-center" ><a className=" text-sm md:text-md text-center" href={`${url}get/${shortId}/`} target="__blank" >{shortId}</a>
+                    <button onClick={copyShortUrl} className="ml-1 "><FaRegCopy /></button></TableCell>
+                <TableCell className="text-center" ><a className="text-center text-sm md:text-md  " href={`${Url}`} target="__blank" >{Url}</a>
+                    <button onClick={copyUrl} className="ml-1 "><FaRegCopy /></button></TableCell>
+                <TableCell className="text-center">{Clicks}</TableCell>
+                <TableCell onClick={() => { deleteFn(shortId) }} className="text-center text-lg md:text-xl cursor-pointer"> <button className="hover:bg-red-500 p-2  rounded-3xl"><MdOutlineDeleteOutline /></button></TableCell>
             </TableRow>
         </TableBody>
     )
