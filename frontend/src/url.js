aefd098a2c67fba3919f339280ac85b13f647ca0
narenidra <narendira2701@gmail.com>
2024-05-18 03:07:09 +0530

    fix url

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,4 +1,4 @@
-// const url = "https://url.narendira.tech/" 54321
-const url = "http://127.0.0.1:54321/"
+const url = "https://url.narendira.tech/"
+// const url = "http://127.0.0.1:54321/" 54321
 const mailUrl = "https://verify.narendira.tech/"
 export { url, mailUrl }
