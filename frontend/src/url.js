3bade7e156c67cf595f6b6498350d8102677e6ff
Narendira27 <41791017+Narendira27@users.noreply.github.com>
2024-12-08 00:18:53 +0530

    Update url.js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,4 +1,4 @@
-const url = "https://url.narendira.tech/";
+const url = "https://url.narendira.in/";
 // const url = "http://127.0.0.1:54321/";
-const mailUrl = "https://verify.narendira.tech/";
+const mailUrl = "https://verify.narendira.in/";
 export { url, mailUrl };
